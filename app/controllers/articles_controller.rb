a003bdc8abe0bd4f623f2292084812a27303b301
emwani1 <edwinmwaniki@Edwins-MBP-2.home>
2018-01-12 19:26:10 -0500

    Restract redundancy and prepare for Deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit,:update,:show,:destroy]
 
   def index
     @articles = Article.all
@@ -21,11 +22,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -34,7 +33,6 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was destroyed"
     redirect_to articles_path
@@ -42,7 +40,6 @@ class ArticlesController < ApplicationController
 
 
   def show
-  @article = Article.find(params[:id])
   end
 
   private
@@ -51,8 +48,8 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title,:description)
   end
 
-
-
-
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 end
