0ef549594589a02eb3e0e3ff6f9d42c8a9089c19
emwani1 <edwinmwaniki@Edwins-MBP-2.home>
2018-01-15 17:08:46 -0500

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  get 'signup', to: 'users#new'
+  resource :users, except: [:new]
   resources :articles
-  resources :users
 end
